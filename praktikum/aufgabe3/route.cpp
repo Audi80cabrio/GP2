627b273466dd5ff68721ab289eb2765059f870c6
jonas <jonas.barnert@gmx.net>
2025-05-04 15:52:11 +0200

    praktikum3 aufgabe a fertig und aufgabe b angefangen(header noch prüfen)

--- praktikum/aufgabe3/route.cpp
+++ praktikum/aufgabe3/route.cpp
@@ -1,2 +1,1 @@
 #include "route.h"
-#include <vector>
--- praktikum/aufgabe3/route.h
+++ praktikum/aufgabe3/route.h
@@ -1,6 +1,28 @@
-#pragma once
+#include <vector>
+#include <utility>
+#include <functional>
 
 class Route {
+    private:
+        std::vector<std::pair<float, float>>* destinations; // Zielkoordinaten
+        float height;                                       // Flughöhe
+        std::function<float(float, float, float, float, float)> dist; // Distanzfunktion
+
+    public:
+        Route(float pHeight, std::function<float(const float, const float, const float, const float, const float)> pDist);
+        Route(const Route& route);
+        ~Route();
 
 
+        //---Getters---
+        std::vector<std::pair<float, float>>& getDestinations();    // Zielkoordinaten getterrrr
+        float getHeight() const;        // Flughöhe getter
+
+        //---Setters---
+        void setHeight(float pHeight);        // Flughöhe setttterrrr
+        void setDist(std::function<float(float, float, float, float, float)> pDist);// Distanzfunktion setterr
+
+        void add(float destX, float destY);     // Zielkoordinaten hinzufügen
+        float distance() const;
+        Route shortestRoute() const; // Kürzeste Route (alle Permutationen prüfen)
 };
